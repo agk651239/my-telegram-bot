58cb4cd387e6a86e9b0db284275af538a7a654cc
A G K <agk651239@gmail.com>
2026-07-16 02:23:06 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,23 +1,33 @@
-
 import time
 from motor.motor_asyncio import AsyncIOMotorClient
-from config import DATABASE_URI, DATABASE_NAME
+# यहाँ config से DATABASE_URI, DATABASE_NAME और ADMIN_IDS तीनों को साथ में import किया है
+from config import DATABASE_URI, DATABASE_NAME, ADMIN_IDS
 
+# MongoDB से कनेक्शन बनाना
 client = AsyncIOMotorClient(DATABASE_URI)
 db = client[DATABASE_NAME]
 
+# वेरिफिकेशन चेक करने का फंक्शन
 async def is_verified(user_id):
-    if user_id in ADMIN_IDS: return True
+    # अगर यूजर एडमिन है, तो हमेशा True (Bypass)
+    if user_id in ADMIN_IDS: 
+        return True
+    
+    # अगर एडमिन नहीं है, तो डेटाबेस में चेक करें
     user = await db.users.find_one({"user_id": user_id})
     return user and user.get("expire_at", 0) > time.time()
 
+# यूजर को वेरीफाई करने का फंक्शन
 async def set_verify(user_id):
+    # 24 घंटे (86400 seconds) का टाइम जोड़कर सेव करना
+    expiry_time = time.time() + 86400
     await db.users.update_one(
         {"user_id": user_id},
-        {"$set": {"expire_at": time.time() + 86400}},
+        {"$set": {"expire_at": expiry_time}},
         upsert=True
     )
 
+# फाइल डेटाबेस में स्टोर करने का फंक्शन
 async def add_file(file_data):
     await db.files.insert_one(file_data)
-  
+    
