7ade8a893c09dc9acfbe5a265d65ff62d413ac27
A G K <agk651239@gmail.com>
2026-07-16 01:57:11 +0530

    Add environment variable configurations to config.py

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+DATABASE_URI = os.environ.get("DATABASE_URI", "")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "bot_db")
+SHORTENER_API = os.environ.get("SHORTENER_API", "")
+SHORTENER_WEBSITE = os.environ.get("SHORTENER_WEBSITE", "")
+VERIFY_EXPIRE = int(os.environ.get("VERIFY_EXPIRE", 86400))
+ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "").split(",")]
+DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", 0))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0))
 
