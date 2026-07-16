caed23cc382b281cf9ee13c4db033793c404e6d9
A G K <agk651239@gmail.com>
2026-07-16 08:54:06 +0530

    Add PORT configuration and update timer settings
    Added PORT configuration and updated timer settings with comments.

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
@@ -6,9 +7,13 @@ DATABASE_URI = os.environ.get("DATABASE_URI", "")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "bot_db")
 ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "").split(",")]
 DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", 0))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0))
 FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", None)
 SHORTENER_API = os.environ.get("SHORTENER_API", "")
 SHORTENER_WEBSITE = os.environ.get("SHORTENER_WEBSITE", "")
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-VERIFY_EXPIRE_TIME = 86400  
-FILE_DELETE_TIME = 3600     
+PORT = int(os.environ.get("PORT", 10000))
+
+# टाइमर सेटिंग्स
+VERIFY_EXPIRE_TIME = 86400  # 24 घंटे (सेकंड में)
+FILE_DELETE_TIME = 3600     # 1 घंटा (सेकंड में)
