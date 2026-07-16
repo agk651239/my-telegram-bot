f46d2d691e8ade4e022a0fff1ed5d032e326e959
A G K <agk651239@gmail.com>
2026-07-16 10:59:52 +0530

    Update FORCE_SUB_CHANNEL to integer type
    Changed FORCE_SUB_CHANNEL to be an integer if set.

--- config.py
+++ config.py
@@ -11,24 +11,23 @@ DATABASE_URI = os.environ.get("DATABASE_URI", "")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "bot_db")
 
 # Channel & Admin Settings
-# ADMIN_IDS को सुरक्षित रूप से हैंडल करने के लिए चेक लगाया गया है
 admin_ids_raw = os.environ.get("ADMIN_IDS", "")
 ADMIN_IDS = [int(x) for x in admin_ids_raw.split(",")] if admin_ids_raw else []
 
 DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", 0))
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0))
-FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", None)
 
-# Shortener Settings (API और Website URL)
+# FORCE_SUB_CHANNEL को int में बदला गया (अगर सेट है)
+force_sub_raw = os.environ.get("FORCE_SUB_CHANNEL", None)
+FORCE_SUB_CHANNEL = int(force_sub_raw) if force_sub_raw else None
+
+# Shortener Settings
 SHORTENER_API = os.environ.get("SHORTENER_API", "")
 SHORTENER_WEBSITE = os.environ.get("SHORTENER_WEBSITE", "")
 
-# Server Port (Render या अन्य होस्टिंग के लिए)
+# Server Port
 PORT = int(os.environ.get("PORT", 10000))
 
 # Time Settings (सेकंड्स में)
-# VERIFY_EXPIRE_TIME: यूजर वेरिफिकेशन कितनी देर तक चलेगा (86400 = 24 घंटे)
 VERIFY_EXPIRE_TIME = int(os.environ.get("VERIFY_EXPIRE_TIME", 86400))
-
-# FILE_DELETE_TIME: फाइल भेजने के बाद कितनी देर में डिलीट होगी (3600 = 1 घंटा)
 FILE_DELETE_TIME = int(os.environ.get("FILE_DELETE_TIME", 3600))
